cdd2298db06c34780a24ea122ea208bc0ee19258
songhyunjeong <songhyunjeong@gmail.com>
2025-04-11 14:49:56 +0900

    훈련

---
+++ src/scanner/ex/ScannerWhileEx3.java
@@ -0,0 +1,32 @@
+package scanner.ex;
+
+import java.util.Scanner;
+
+public class ScannerWhileEx3 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int sum = 0;
+        int count = 0;
+
+        System.out.println("숫자를 입력하세요. 입력을 중단하려면 -1을 입력하세요: ");
+        while (true) {
+            int n = input.nextInt();
+            if (n == -1) {
+                break;
+            }
+            sum += n;
+            count++;
+        }
+
+        /*
+        while ((n = input.nextInt()) != -1) {
+            sum += n;
+            count++;
+        }
+        */
+
+        System.out.println("입력한 숫자들의 합계: " + sum);
+        System.out.println("입력한 숫자들의 평균: " + ((double) sum / count));
+    }
+}
